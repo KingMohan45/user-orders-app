528d606e8ed089f174a6a1e1bc1199f257d6feea
Mohan Kumar R <kingmohan45@kings-mac.local>
2024-03-01 18:33:04 +0530

    Updated backend base url

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-export const API_URL = 'http://localhost:8000';
-export const WS_API_URL = 'ws://localhost:8000';
+export const API_URL = 'http://127.0.0.1:8000';
+export const WS_API_URL = 'ws://127.0.0.1:8000';
 export const USERS_ENDPOINT = '/users';
 export const ORDERS_ENDPOINT = '/orders';
